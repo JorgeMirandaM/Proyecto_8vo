9657e02e26b0ccada2d5f9483a03415b74180207
Josue Hiram Alvarez Martinez <josue.h.alvarez.martinez@gmail.com>
2022-04-21 21:39:16 -0500

    Bebes sin refrescar pag, footer, icon

--- Datos_Personales.js
+++ Datos_Personales.js
@@ -1,26 +1,77 @@
 
-import { collection, addDoc, getDocs,deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
+import { collection, addDoc, getDocs,deleteDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
 
 import {db} from './firebase.js';
 
+const datosContainer = document.getElementById('datos-container');
+let bebes = [];
+
+function babyCards(doc,cont){
+  datosContainer.innerHTML+=`
+    <div class="col">
+    <div class="card mt-3">
+          <div class="card-body">
+            <div class="row">
+            <div class="col-10">
+            <h5 class="card-title">${doc.data().Cunero}</h5>
+          </div>
+          <div class="col-2">
+          <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal${cont}">
+          <i class="fas fa-info-circle"></i>
+          <button type="button" class="btn ml-3 btn-danger" data-id="${doc.id}"> <i class="fas fa-trash"></i></button>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
 
 
-//Metodo para eliminar
+  <div class="modal fade" id="exampleModal${cont}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">  
+        <h4 class="text-center mt-3">
+          <i class="fas fa-baby"></i> Información del bebé: ${doc.data().Cunero}
+        </h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <legend class="text-dark " style="font-size: medium"><b>Nombre(s) de Padre/Madre:</b> ${doc.data().NombrePadres} </legend>
+        <legend class="text-dark " style="font-size: medium"><b>Apellidos:</b> ${doc.data().ApellidoP}  ${doc.data().ApellidoM}</legend>
+        <legend class="text-dark " style="font-size: medium"><b>Fecha de nacimiento:</b> ${doc.data().Fecha} </legend>
+        <legend class="text-dark " style="font-size: medium"><b>Hora de nacimiento:</b> ${doc.data().Hora} </legend>
+        <legend class="text-dark " style="font-size: medium"><b>Genero:</b> ${doc.data().Genero} </legend>
+        <legend class="text-dark " style="font-size: medium"><b>Peso:</b> ${doc.data().Peso} Kg</legend>
+      </div>
+      <div class="modal-footer">
+          <a href="./Datos_Médicos.html" type="button" class="btn btn-secondary" aria-pressed="true">Datos</a>
 
-function deleteDocument (){
-    const btns = document.querySelectorAll('.btn-danger');
-    btns.forEach(btn => {
-      btn.addEventListener('click', async (e) => {
-         console.log(e.target.dataset.id);
-         await deleteDoc(doc(db, 'Bebes', e.target.dataset.id));
-      })
-    })
+      </div>
+    </div>
+  </div>
+  </div>
+  `
+}
+
+ //Obtener los datos de la base de datos
+ 
+ window.addEventListener('DOMContentLoaded', async e =>{
+  try {
+    bebes = await getDocs(collection(db, "Bebes"));
+    let cont=0;
+    bebes.forEach((doc) => {
+      babyCards(doc,cont);
+    cont++;
+    deleteDocument();
+    });    
+  } catch (error) {
+      console.log(error)
   }
-  
+});
+
    //Almacenar los datos en la base de datos
   const informationForm = document.getElementById('information-form');
- 
-  informationForm.addEventListener('submit', e =>{
+  informationForm.addEventListener('submit', async e =>{
     e.preventDefault();
     var time = "";
     var selected = "";
@@ -44,81 +95,47 @@ function deleteDocument (){
         Hora: time,
         Cunero: str
       });
-      console.log("Document written with ID: ", docRef.id);
+      bebes = [];
+        datosContainer.innerHTML="";
+        bebes = await getDocs(collection(db, "Bebes"));
+        let cont=0;
+        bebes.forEach((doc) => {
+        babyCards(doc,cont);
+        cont++;
+        deleteDocument();
+    });
     } catch (e) {
       console.error("Error adding document: ", e);
     }
     limpiarFormulario();
   });
-    function limpiarFormulario() {
-    document.getElementById("information-form").reset();
-  }
-  
-  //Obtener los datos de la base de datos
-  
-  window.addEventListener('DOMContentLoaded', async e =>{
-    //console.log(e);
-  
-  
-  try {
-    const bebes = await getDocs(collection(db, "Bebes"));
-    const datosContainer = document.getElementById('datos-container');
-    let cont=0;
-    bebes.forEach((doc) => {
-      datosContainer.innerHTML+=`
-      <div class="col">
-      <div class="card mt-3">
-            <div class="card-body">
-              <div class="row">
-              <div class="col-10">
-              <h5 class="card-title">${doc.data().Cunero}</h5>
-            </div>
-            <div class="col-2">
-            <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal${cont}">
-            <i class="fas fa-info-circle"></i>
-            <button type="button" class="btn ml-3 btn-danger" data-id="${doc.id}"> <i class="fas fa-trash"></i></button>
-          </div>
-         </div>
-        </div>
-      </div>
-    </div>
-    
-    
-    <div class="modal fade" id="exampleModal${cont}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">  
-          <h4 class="text-center mt-3">
-            <i class="fas fa-baby"></i> Información del bebé: ${doc.data().Cunero}
-          </h4>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <legend class="text-dark " style="font-size: medium"><b>Nombre(s) de Padre/Madre:</b> ${doc.data().NombrePadres} </legend>
-          <legend class="text-dark " style="font-size: medium"><b>Apellidos:</b> ${doc.data().ApellidoP}  ${doc.data().ApellidoM}</legend>
-          <legend class="text-dark " style="font-size: medium"><b>Fecha de nacimiento:</b> ${doc.data().Fecha} </legend>
-          <legend class="text-dark " style="font-size: medium"><b>Hora de nacimiento:</b> ${doc.data().Hora} </legend>
-          <legend class="text-dark " style="font-size: medium"><b>Genero:</b> ${doc.data().Genero} </legend>
-          <legend class="text-dark " style="font-size: medium"><b>Peso:</b> ${doc.data().Peso} Kg</legend>
-        </div>
-        <div class="modal-footer">
-            <a href="./Datos_Médicos.html" type="button" class="btn btn-secondary" aria-pressed="true">Datos</a>
-    
-        </div>
-      </div>
-     </div>
-    </div>
-    `
-    cont++;
-    deleteDocument();
+
+
+//Metodo para eliminar
+function deleteDocument (){
+  const btns = document.querySelectorAll('.btn-danger');
+  btns.forEach(btn => {
+    btn.addEventListener('click', async (e) => {
+      await deleteDoc(doc(db, 'Bebes', e.target.dataset.id));
+      bebes = [];
+      datosContainer.innerHTML="";
+      bebes = await getDocs(collection(db, "Bebes"));
+      let cont=0;
+      bebes.forEach((doc) => {
+      babyCards(doc,cont);
+      cont++;
+      deleteDocument();
     });
-  
-      
-  } catch (error) {
-      console.log(error)
-  }
-  
-});
+    })
+  })
+} 
+
+//Resetear formulario
+function limpiarFormulario() {
+  document.getElementById("information-form").reset();
+} 
+
+
 
   
 
